034dc061fb9f1f7162af44d97dd97bbf3e90ba87
Charan <your-charanchowdary270@gmail.com>
2026-04-25 21:40:51 +0530

    backend ready

---
+++ models/Project.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+const ProjectSchema = new mongoose.Schema({
+  name: String,
+  description: String
+});
+
+module.exports = mongoose.model("Project", ProjectSchema);
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const Project = require("./models/Project");
+
+// 🔗 Connect MongoDB
+mongoose.connect("mongodb://charanchowdary270_db_user:charan2007@ac-rs25la0-shard-00-00.ibqtpjk.mongodb.net:27017,ac-rs25la0-shard-00-01.ibqtpjk.mongodb.net:27017,ac-rs25la0-shard-00-02.ibqtpjk.mongodb.net:27017/?ssl=true&replicaSet=atlas-agvmhn-shard-0&authSource=admin&appName=Cluster0")
+  .then(() => console.log("MongoDB Atlas Connected"))
+  .catch(err => console.log(err));
+
+// 📥 GET projects
+app.get("/projects", async (req, res) => {
+  const projects = await Project.find();
+  res.json(projects);
+});
+
+// 📤 ADD project
+app.post("/projects", async (req, res) => {
+  const newProject = new Project(req.body);
+  await newProject.save();
+  res.json(newProject);
+});
+
+app.listen(5001, () => {
+  console.log("Server running on port 5001");
+});
